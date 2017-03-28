08cf4ace8ba00bc359b78e441047b8779ce93fa4
BUCKNALL Laura D <b012501c@student.staffs.ac.uk>
2017-03-28 16:45:00 +0100

    serviceworker update

--- serviceworker.js
+++ serviceworker.js
@@ -1,18 +1,37 @@
 var CACHE_NAME = 'gih-cache-v5';
+var BASE_PATH = '/dfmd/';
 var CACHED_URLS = [
   // Our HTML
-  'first.html',
+  BASE_PATH + 'first.html',
   // Stylesheets and fonts
-    'styles.css',
-    'mystyles.css',
-    'min-style.css',
-    'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
+    BASE_PATH + 'styles.css',
+    BASE_PATH + 'mystyles.css',
+    BASE_PATH + 'min-style.css',
+    //BASE_PATH + 'https://fonts.googleapis.com/icon?family=Material+Icons',
+    //BASE_PATH + 'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
   // JavaScript
-    'material.js',
+    BASE_PATH + 'material.js',
   // Images
-    'paddy.jpg',
-    'eventsimage/example-01.jpg'
+    BASE_PATH + 'appimages/paddy.jpg',
+    BASE_PATH + 'appimages/dino.png',
+    BASE_PATH + 'appimages/jack.jpg',
+    BASE_PATH + 'appimages/favicon.ico',
+    BASE_PATH + 'appimages/favicon-16x16.png',
+    BASE_PATH + 'appimages/favicon-32x32.png',
+    BASE_PATH + 'appimages/favicon-96x96.png',
+    BASE_PATH + 'appimages/ms-icon-70x70.png',
+    BASE_PATH + 'appimages/ms-icon-144x144.png',
+    BASE_PATH + 'appimages/ms-icon-150x150.png',
+    BASE_PATH + 'appimages/ms-icon-310x310.png',
+    BASE_PATH + 'eventsimages/example-blog01.jpg',
+    BASE_PATH + 'eventsimages/example-blog02.jpg',
+    BASE_PATH + 'eventsimages/example-blog03.jpg',
+    BASE_PATH + 'eventsimages/example-blog04.jpg',
+    BASE_PATH + 'eventsimages/example-blog05.jpg',
+    BASE_PATH + 'eventsimages/example-blog06.jpg',
+    BASE_PATH + 'eventsimages/example-blog07.jpg',
+    BASE_PATH + 'eventsimages/example-blog08.jpg',
+    BASE_PATH + 'eventsimages/example-blog09.jpg'
 ];
 
 self.addEventListener('install', function(event) {
@@ -25,18 +44,32 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get('accept').includes('text/html')) {
-          return caches.match('first.html');
-        }
-      });
-    })
-  );
+  var requestURL = new URL(event.request.url);
+  if (requestURL.pathname === BASE_PATH + 'first.html') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match('first.html').then(function(cachedResponse) {
+          var fetchPromise = fetch('first.html').then(function(networkResponse) {
+            cache.put('first.html', networkResponse.clone());
+            return networkResponse;
+          });
+          return cachedResponse || fetchPromise;
+        });
+      })
+    );
+  } else if (
+    CACHED_URLS.includes(requestURL.href) ||
+    CACHED_URLS.includes(requestURL.pathname) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+        })
+      })
+    );
+  }
 });
+
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
